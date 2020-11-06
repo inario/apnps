8303471693788f1ae060898d02904caef90884e7
inario <inaciojr2001@gmail.com>
2020-11-06 08:17:22 -0300

    Arquivo index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/',function(req,res){
+    res.send("Olá Mundo")
+})
+
+app.listen(3000,function(){
+    console.log("Conexão Inicializada na porta 3000")
+})
