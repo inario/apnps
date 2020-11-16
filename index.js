7512bc65d73217c89527aad0b673c9e6d5a078c8
inario <inaciojr2001@gmail.com>
2020-11-16 10:12:52 -0300

    rota mensagem

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/',function(req,res){
     res.send("Olá Mundo")
 })
 
+app.get('/mensagem',function(req,res){
+    res.send("Essa mensagem é automática.")
+
+})
+
 app.listen(3000,function(){
     console.log("Conexão Inicializada na porta 3000")
 })
